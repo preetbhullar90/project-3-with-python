96ce350e4dd81178180819745a017c83db1bf24b
preetbhullar90 <gurpreet.singhb90@gmail.com>
2021-12-16 15:37:20 +0000

    Add a function for user input and for welcome message

--- run.py
+++ run.py
@@ -27,3 +27,55 @@ def get_all_words():
     for item in lists:
         word_list = item
         return word_list
+
+
+def welcome_game():
+    """
+    Get name input from the users.
+    while loop runs until the user does not provide a valid input.
+    If the input is valid the user will see a welcome message with
+    the username and hangman text.
+    """
+    print("\n")
+    global NAME
+    NAME = input(Fore.YELLOW + Style.BRIGHT + "Please enter your name: ")
+    print("\n")
+    print(Fore.YELLOW + Style.BRIGHT + "wait...")
+    time.sleep(2)
+    os.system("cls" if os.name == "nt" else "clear")
+    print("\n")
+    while not NAME.isalpha() or len(NAME) < 2:
+        print("\n")
+        print(Fore.YELLOW + Style.BRIGHT + "wait...")
+        time.sleep(2)
+        os.system("cls" if os.name == "nt" else "clear")
+        print(
+            Fore.RED + "Sorry, Your name is invalid, please "
+            "enter your full name in letters\n"
+        )
+        print("\n")
+        NAME = input(Fore.YELLOW + Style.BRIGHT + "Please enter your name: ")
+    else:
+        print("\n")
+        print(Fore.YELLOW + Style.BRIGHT + "Game loading...")
+        print("\n")
+        time.sleep(2)
+        os.system("cls" if os.name == "nt" else "clear")
+        print("\033[1;34m")
+        print(
+            """
+        ██╗  ██╗ █████╗ ███╗   ██╗ ██████╗ ███╗   ███╗ █████╗ ███╗   ██╗
+        ██║  ██║██╔══██╗████╗  ██║██╔════╝ ████╗ ████║██╔══██╗████╗  ██║
+        ███████║███████║██╔██╗ ██║██║  ███╗██╔████╔██║███████║██╔██╗ ██║
+        ██╔══██║██╔══██║██║╚██╗██║██║   ██║██║╚██╔╝██║██╔══██║██║╚██╗██║
+        ██║  ██║██║  ██║██║ ╚████║╚██████╔╝██║ ╚═╝ ██║██║  ██║██║ ╚████║
+        ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝
+        """
+        )
+        print("\n\n")
+        print(
+            Fore.BLUE + Style.BRIGHT + "Hello, " + NAME.capitalize(),
+            "Welcome to hangman!",
+        )
+        time.sleep(0.5)
+        random_word()
