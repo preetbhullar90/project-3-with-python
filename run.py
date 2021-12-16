346181951ea7195501f45663e1979eba8a2e41e1
preetbhullar90 <gurpreet.singhb90@gmail.com>
2021-12-16 17:57:03 +0000

    create new function display_hangman to show image of hangman to the users

--- run.py
+++ run.py
@@ -332,3 +332,81 @@ def play_game(words):
         )
         print("\n")
         playgame_again()
+
+
+def display_hangman(user_tries):
+    """
+    This function shows the image in the game according to the users input.
+    If user selects the wrong answer then
+    they will get the next image out of the 6 progressions of the hangman.
+    This function runs until the user runs out of all their tries.
+    """
+    image_stages = [
+        """
+
+        --------
+        |      |
+        |      O
+        |     \\|/
+        |      |
+        |     / \\
+        -
+        """,
+        """
+        --------
+        |      |
+        |      O
+        |     \\|/
+        |      |
+        |     /
+        -
+        """,
+        """
+        --------
+        |      |
+        |      O
+        |     \\|/
+        |      |
+        |
+        -
+        """,
+        """
+        --------
+        |      |
+        |      O
+        |     \\|
+        |      |
+        |
+        -
+        """,
+        """
+        --------
+        |      |
+        |      O
+        |      |
+        |      |
+        |
+        -
+        """,
+        """
+        --------
+        |      |
+        |      O
+        |
+        |
+        |
+        -
+        """,
+        """
+
+        --------
+        |      |
+        |
+        |
+        |
+        |
+        -
+
+        """,
+    ]
+    return image_stages[user_tries]
