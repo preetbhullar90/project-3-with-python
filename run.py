7b8575cdd04238059ec67be87b68ea1c2326a5b5
preetbhullar90 <gurpreet.singhb90@gmail.com>
2021-12-16 15:22:04 +0000

    Import gspread sheet

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+import colorama, os,random,google-auth and gspread link with run.py.
+"""
+import os
+import random
+import gspread
+from google.oauth2.service_account import Credentials
+from colorama import Fore, Style
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive",
+]
+CREDS = Credentials.from_service_account_file("creds.json")
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open("hangman_game")
